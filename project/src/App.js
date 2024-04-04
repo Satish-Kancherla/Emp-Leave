3ec7e0c3a1814d602062dcdea3668f24543e6ff0
nagakancherla123@gmail.com <nagakancherla123@gmail.com>
2024-04-04 14:54:55 +0530

    leave components commented

--- project/src/App.js
+++ project/src/App.js
@@ -6,10 +6,10 @@ import ViewList from "./Emp-comp/ViewList";
 
 import {BrowserRouter ,Routes,Route} from "react-router-dom";
 
-import AdminPage1 from "./Leave-comp/AdminPage";
+// import AdminPage1 from "./Leave-comp/AdminPage";
 import Jan1 from "./Leave-comp/Jan";
-import Update1 from "./Leave-comp/Update";
-import EmployeeData1 from "./Leave-comp/EmployeeData";
+// import Update1 from "./Leave-comp/Update";
+// import EmployeeData1 from "./Leave-comp/EmployeeData";
 import Emplist from "./Leave-comp/Emplist";
 
 
@@ -23,12 +23,12 @@ function App() {
         <Route path="/employee" element={<EmployeeData />}/>
         <Route path="/filter" element={<Jan />} />
         <Route path="/update/:id" element={<Update/>}/>
-        <Route path="viewlist" element={<ViewList />}/>
+        <Route path="/viewlist" element={<ViewList />}/>
 
-        <Route path="/leave" element={ <AdminPage1 />}/>
-        <Route path="/employeelist" element={<EmployeeData1 />}/>
+        {/* <Route path="/leave" element={ <AdminPage1 />}/> */}
+        {/* <Route path="/employeelist" element={<EmployeeData1 />}/> */}
         <Route path="/filterlist" element={<Jan1 />} />
-        <Route path="/updatelist/:id" element={<Update1/>}/>
+        {/* <Route path="/updatelist/:id" element={<Update1/>}/> */}
         <Route path="/emplist" element={<Emplist />}/>
         
       </Routes>
